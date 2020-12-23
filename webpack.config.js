3333ba19a43498a49a0b641ee5e632435240b89c
Shashwat <shashwatrathod22@gmail.com>
2020-12-23 22:18:27 +0530

    init; config babel,webpack success

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+const HWP = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "/src/index.js"),
+  output: {
+    filename: "build.js",
+    path: path.join(__dirname, "/dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HWP({
+      template: path.join(__dirname, "/public/index.html"),
+    }),
+  ],
+};
